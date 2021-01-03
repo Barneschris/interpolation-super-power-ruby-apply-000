512d3cead3a7dff083691444b3a11635f829baa0
lite-panel-4424 <barneschristopherh@gmail.com>
2021-01-03 18:40:11 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def #display_rainbow(colors)
   colors = [red = "R: red",orange = "O: orange",yello = "Y: yellow",green = "G: green",blue = "B: blue",indigo = "I: indigo",violet = "V: violet"]
-  puts "colors[]"
+  puts "colors #{red}"
 end
 
