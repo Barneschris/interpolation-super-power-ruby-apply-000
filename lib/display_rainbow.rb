4fe40a738a3bd7a76a6fa447612717e14f3f8ff9
lite-panel-4424 <barneschristopherh@gmail.com>
2021-01-03 18:18:10 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,4 @@
-def display_rainbow(colors)
+def display_rainbow(colors ="R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet")
   puts "#{colors[R,O,Y,G,B,I,V]"
 end
 
-R = ["red"]
-O = ["orange"]
-Y = ["yellow"]
-G = ["green"]
-B = ["blue"]
-I = ["indigo"]
-V = ["violet"]
